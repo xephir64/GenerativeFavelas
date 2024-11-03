1922341e1172391e7331c444917e8aff293edec0
xephir64 <chaubet.arnaud@gmail.com>
2024-11-03 12:02:08 +0100

    updated dependencies

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,6 +8,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 #include "Camera.h"
 #include "Config.h"
 #include "Geometry/Door.h"
